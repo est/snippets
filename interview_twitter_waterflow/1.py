2d527833ddf8352d7dee3d72d43dfc588509245b
est <electronicstar@126.com>
2013-11-11 12:19:44 +0800

    twitter interview waterflow question

---
+++ interview_twitter_waterflow/1.py
@@ -0,0 +1,7 @@
+#!/usr/env/bin python
+
+
+# http://www.reddit.com/r/programming/comments/1qc8iw/a_functional_solution_to_twitters_waterflow/
+# http://philipnilsson.github.io/Badness10k/articles/waterflow/
+
+
