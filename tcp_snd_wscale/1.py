1838f1363b2849eae88fcd5b8b8bc576cd2a9fe3
est <electronicstar@126.com>
2014-01-22 09:24:38 +0800

    wrap up

--- tcp_snd_wscale/1.py
+++ tcp_snd_wscale/1.py
@@ -5,8 +5,8 @@
 import socket
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 10)
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10)
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 10)
 s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
 s.connect(('google.com', 80))
 snd = 'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
--- tcp_snd_wscale/readme.md
+++ tcp_snd_wscale/readme.md
@@ -5,3 +5,4 @@ Reference
 
  - http://developer.nokia.com/Community/Wiki/Set_TCP_Window_Size
  - http://www.vonwelch.com/report/tcp_windows/
+ - http://stackoverflow.com/a/8579250/41948
