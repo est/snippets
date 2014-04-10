f3e59cfd2bdcd5eae940d2a36f5ce5a80f47055d
est <electronicstar@126.com>
2014-04-10 10:39:37 +0800

    filter

--- aws_report_ggplot/report.py
+++ aws_report_ggplot/report.py
@@ -5,7 +5,7 @@ import datetime
 writer = csv.writer(open('report_out.csv', 'w'))
 
 
-fmt1 = '%d/%m/%y %H:%M:%s'
+fmt1 = '%m/%d/%y %H:%M:%S'
 
 with open('report.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -14,14 +14,19 @@ with open('report.csv') as csvfile:
     for i, row in enumerate(reader):
         # Service, Operation, UsageType, Resource, StartTime, EndTime, UsageValue
         try:
+            operation = row[1]
             usage_type = row[2]
             resource = row[3]
             time = row[4]
-            if i>1:
-                time = datetime.datetime.strptime(fmt1, time)
-                time = time.date.isoformat()
+            if i>0:
+                time = datetime.datetime.strptime(time, fmt1)
+                time = time.date().isoformat()
             usage_value = row[6]
-            writer.writerow([usage_type, resource, time, usage_value])
+            if usage_type == 'DataTransfer-Out-Bytes' and operation == 'GetObject':
+                
+
+
+                writer.writerow([operation, usage_type, resource, time, usage_value])
         except IndexError:
             print type(row), row
 
