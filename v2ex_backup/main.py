45e52a127941a929609e18da09340d6515cba106
est <electronicstar@126.com>
2016-11-15 17:05:39 +0800

    add v2ex replies

---
+++ v2ex_backup/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import urllib2
+import time
+
+url = 'https://www.v2ex.com/member/est/replies?p=%s'
+
+
+for x in xrange(313, 0, -1):
+    try:
+        r = urllib2.urlopen(url % x).read()
+    except:
+        continue
+    open('%.html', 'w').write(r)
+    time.sleep(2)
