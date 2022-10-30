2f1c9ca0194f890067d1f7b872cade2e9841776f
est <electronicstar@126.com>
2022-10-30 21:53:44 +0800

    cringe

--- perm_sort/1.py
+++
@@ -1,3 +0,0 @@
-import random, itertools
-
-l = [random.randint(0, 100) for x in xrange(10)]
--- unicode_var/1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/env/bin python
-# coding: utf8
-
-# inspired from http://javascript.reddit.com/comments/1v6irn/_/cepbims
-
-d = {}
-d['а'] = 1
-d['a'] = 2
-print (d['а'], d['a'], d['а']==d['a'])
