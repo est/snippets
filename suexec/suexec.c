f0c7728312a6076bc315a592af0f20a5f87ef78d
est <electronixtar@gmail.com>
2026-01-15 22:10:59 +0800

    lmao

--- suexec/suexec.c
+++ suexec/suexec.c
@@ -3,10 +3,13 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        write(2, "usage: execwrap <program> [args...]\n", 37);
+        char buf[1024];
+        int len = snprintf(buf, sizeof(buf), "usage: %s <program> [args...]\n", argv[0]);
+        write(2, buf, len);
         return 1;
     }
 
+    // We're already running as root via setuid, so we can execute directly
     execv(argv[1], &argv[1]);
 
     // Only reached if exec fails
